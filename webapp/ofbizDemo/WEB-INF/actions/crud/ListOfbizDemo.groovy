be952e8e4cffca87c2ac9c7534c8bb85113872d4
Ashish Vijaywargiya <vijaywargiya.ashish@gmail.com>
2024-03-18 15:11:35 +0530

    Minor changes pushed. Added some label statements in Screen and also added the DSL code in groovy file. I have commented out the old code which is written in OFBiz tutorial.

--- webapp/ofbizDemo/WEB-INF/actions/crud/ListOfbizDemo.groovy
+++ webapp/ofbizDemo/WEB-INF/actions/crud/ListOfbizDemo.groovy
@@ -1,4 +1,11 @@
-ofbizDemoTypes = delegator.findList("OfbizDemoType", null, null, null, null, false);
-context.ofbizDemoTypes = ofbizDemoTypes;
-ofbizDemoList = delegator.findList("OfbizDemo", null, null, null, null, false);
+//ofbizDemoTypes = delegator.findList("OfbizDemoType", null, null, null, null, false);
+//context.ofbizDemoTypes = ofbizDemoTypes;
+//ofbizDemoList = delegator.findList("OfbizDemo", null, null, null, null, false);
+//context.ofbizDemoList = ofbizDemoList;
+
+// New way of doing the above in Groovy - Use DSL Capabilities
+context.ofbizDemoTypes = from("OfbizDemoType").queryList()
+
+ofbizDemoList = from("OfbizDemo").queryList()
+logInfo("The value of ofbizDemoList is ====== " + ofbizDemoList)
 context.ofbizDemoList = ofbizDemoList;
